6562e0eee267cbb82ce10228e76962df341501e9
Nicolas <marty.nicolas11@laposte.net>
2019-03-25 16:27:12 +0100

    Exo 2

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -16,48 +16,74 @@ import static org.junit.Assert.*;
  */
 public class Lambda_02_Test {
 
-    // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
-    }
-    // end::PersonToAccountMapper[]
+	// tag::PersonToAccountMapper[]
+	interface PersonToAccountMapper {
+		Account map(Person p);
+	}
+	// end::PersonToAccountMapper[]
 
-    // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
-        // TODO implémenter la méthode
-        return null;
-    }
-    // end::map[]
+	// tag::map[]
+	private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+		// TODO implémenter la méthode
 
+		List<Account> result = new ArrayList<>();
 
-    // tag::test_map_person_to_account[]
-    @Test
-    public void test_map_person_to_account() throws Exception {
+		for (Person p : personList) {
+			result.add(mapper.map(p));
+		}
 
-        List<Person> personList = Data.buildPersonList(100);
+		return result;
+	}
+	// end::map[]
 
-        // TODO transformer la liste de personnes en liste de comptes
-        // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+	// tag::test_map_person_to_account[]
+	@Test
+	public void test_map_person_to_account() throws Exception {
 
-        assertThat(result, hasSize(personList.size()));
-        assertThat(result, everyItem(hasProperty("balance", is(100))));
-        assertThat(result, everyItem(hasProperty("owner", notNullValue())));
-    }
-    // end::test_map_person_to_account[]
+		List<Person> personList = Data.buildPersonList(100);
 
-    // tag::test_map_person_to_firstname[]
-    @Test
+		// TODO transformer la liste de personnes en liste de comptes
+		// TODO tous les objets comptes ont un solde à 100 par défaut
+		PersonToAccountMapper mapper = new PersonToAccountMapper() {
+
+			@Override
+			public Account map(Person p) {
+
+				Account account = new Account();
+				account.setOwner(p);
+				account.setBalance(100);
+
+				return account;
+			}
+
+		};
+
+		List<Account> result = map(personList, mapper);
+
+		assertThat(result, hasSize(personList.size()));
+		assertThat(result, everyItem(hasProperty("balance", is(100))));
+		assertThat(result, everyItem(hasProperty("owner", notNullValue())));
+	}
+	// end::test_map_person_to_account[]
+
+	// tag::test_map_person_to_firstname[]
+	@Test
     public void test_map_person_to_firstname() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        
+        List<String> result = new ArrayList<>();
+
+        for (Person p : personList) {
+        	result.add(p.getFirstname());
+        }
+        
 
         assertThat(result, hasSize(personList.size()));
         assertThat(result, everyItem(instanceOf(String.class)));
         assertThat(result, everyItem(startsWith("first")));
     }
-    // end::test_map_person_to_firstname[]
+	// end::test_map_person_to_firstname[]
 }
