52b154de791636b9fa10404ac1848d4370f38a0a
Nicolas <marty.nicolas11@laposte.net>
2019-03-26 16:09:28 +0100

    exo 1

--- src/test/java/java8/data/Data.java
+++ src/test/java/java8/data/Data.java
@@ -1,15 +1,21 @@
 package java8.data;
 
-
 import java.util.List;
+
 import java.util.stream.Collectors;
+
 import java.util.stream.IntStream;
 
 public class Data {
 
-    public static List<Person> buildPersonList(int nb) {
-        return IntStream.rangeClosed(1,nb)
-                .mapToObj(i -> new Person("first_" + i, "last_" + i, i, i % 9 == 0 ? "test": "password"+i))
-                .collect(Collectors.toList());
-    }
+	public static List<Person> buildPersonList(int nb) {
+
+		return IntStream.rangeClosed(1, nb)
+
+				.mapToObj(i -> new Person("first_" + i, "last_" + i, i, i % 9 == 0 ? "test" : "password" + i))
+
+				.collect(Collectors.toList());
+
+	}
+
 }
--- src/test/java/java8/ex01/Optional_01_Test.java
+++ src/test/java/java8/ex01/Optional_01_Test.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -42,7 +43,8 @@ public class Optional_01_Test {
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10
-        Optional<Person> result = null;
+        
+        Optional<Person> result = Optional.of(this.find(personList, t -> t.getAge() == 10));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(true));
@@ -59,7 +61,7 @@ public class Optional_01_Test {
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 400
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.ofNullable(this.find(personList, t -> t.getAge() == 400));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(false));
@@ -73,9 +75,10 @@ public class Optional_01_Test {
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10 et firstname == "last_10"
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.ofNullable(this.find(personList, t -> t.getAge() == 10 && t.getFirstname().equals("last_10")));
 
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception NotFountException si non trouvé
+        result.orElseThrow(() -> new NotFountException());
     }
 
     @Test
@@ -90,7 +93,7 @@ public class Optional_01_Test {
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate, T defaultValue)
         // TODO predicate => age == 400
-        Person result = null;
+        Person result = Optional.ofNullable(this.find(personList, t -> t.getAge() == 400)).orElse(defaultValue);
 
         assertThat(result, notNullValue());
         assertThat(result, hasProperty("firstname", is("DEFAULT")));
