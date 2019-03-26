a6d0547cee5fab83e26aecf476eb177e3999a10c
Nicolas <marty.nicolas11@laposte.net>
2019-03-26 14:27:35 +0100

    exo 6

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -26,8 +26,10 @@ public class Function_06_Test {
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+
+    	Person p = supplier.get();
+
+        return "[age=" + p.getAge() + "]";
     }
     // end::formatAge[]
 
@@ -35,7 +37,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(() -> new Person("", "", 35, ""));
 
         assertThat(result, is("[age=35]"));
     }
@@ -47,7 +49,7 @@ public class Function_06_Test {
         expectedException.expectMessage("require non null object");
 
         // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier = null;
+        Supplier<String> supplier = () -> new String("require non null object");
 
         // Avec un paramètre null, cette méthode déclenche un NullPointerException
         Objects.requireNonNull(null, supplier);
